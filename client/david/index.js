e07787f319649ec8125f7ef60d398d1168069d3d
David Ahmadov <ahmedavid@gmail.com>
2024-06-14 12:25:19 +0000

    ui better

--- client/david/index.js
+++ client/david/index.js
@@ -1,21 +1,32 @@
 const form = document.querySelector("form")
 const answer = document.querySelector("#answer")
+const promptInput = document.querySelector("#promptInput")
+const promptBtn = document.querySelector("#promptBtn")
+const promptBtnSpinner = document.querySelector("#promptBtnSpinner")
+
+promptBtnSpinner.style.visibility = "hidden"
+
+let buttonDisabled = true
 
 function sendQuery() {
   // fetch("http://localhost:8080", {})
   const baseUrl = 'http://195.242.25.207:5000/api'
 
+  console.log("PROMPT: ", promptInput.value)
+
   // Parameters to be included in the query string
   const params = {
-      query: 'What is the dresscode?',
-      param2: 'value2',
-      param3: 'value3'
+      query: promptInput.value
   };
   const url = new URL(baseUrl);
   url.search = new URLSearchParams(params).toString();
 
   // Log the final URL (optional)
   console.log('Final URL:', url.toString());
+
+  promptBtn.disabled = true
+  promptBtn.style.visibility = "hidden"
+  promptBtnSpinner.style.visibility = "visible"
 
   // Send the GET request using fetch
   fetch(url)
@@ -29,9 +40,15 @@ function sendQuery() {
           console.log('Data received:', data); // Handle the data
 
           answer.textContent = data
+          promptBtn.disabled = false
+          promptBtn.style.visibility = "visible"
+          promptBtnSpinner.style.visibility = "hidden"
       })
       .catch(error => {
           console.error('There was a problem with the fetch operation:', error);
+          promptBtn.disabled = false
+          promptBtn.style.visibility = "visible"
+          promptBtnSpinner.style.visibility = "hidden"
       });
 }
 
